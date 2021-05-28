6666e8d83419a2cc7c6e9eedaebb580ba74917ef
danillllborisov <54914198+danillllborisov@users.noreply.github.com>
2021-05-28 14:57:58 -0400

    Fullstack Initial
    Complete front end.
    Complete MSSQL connection (local)
    Userlogin/Adminlogin backe end

---
+++ PortfolioWebASP/PortfolioWebASP/Site1.Master.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PortfolioWebASP
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Session["role"].Equals(" "))
+                {
+                    //USer links 
+                    LinkButton1.Visible = true; //user login
+                    LinkButton2.Visible = true; //sign up
+
+                    LinkButton3.Visible = false; // logout
+                    LinkButton7.Visible = false; // username 
+
+
+                    //Admin links
+                    LinkButton6.Visible = true; //admin login
+
+                    LinkButton11.Visible = false; //author management 
+                    LinkButton12.Visible = false; //publisher management 
+                    LinkButton8.Visible = false; //bookinventory
+                    LinkButton9.Visible = false; //book issuing
+                    LinkButton10.Visible = false; //member managemt 
+                }
+                else if (Session["role"].Equals("user"))
+                {
+                    //USer links 
+                    LinkButton1.Visible = false; //user login
+                    LinkButton2.Visible = false; //sign up
+
+                    LinkButton3.Visible = true; // logout
+                    LinkButton7.Visible = true; // username 
+                    LinkButton7.Text ="Hello " + Session["username"].ToString();
+
+                    //Admin links
+                    LinkButton6.Visible = true; //admin login
+
+                    LinkButton11.Visible = false; //author management 
+                    LinkButton12.Visible = false; //publisher management 
+                    LinkButton8.Visible = false; //bookinventory
+                    LinkButton9.Visible = false; //book issuing
+                    LinkButton10.Visible = false; //member managemt
+                }
+                else if (Session["role"].Equals("admin"))
+                {
+                    //USer links 
+                    LinkButton1.Visible = false; //user login
+                    LinkButton2.Visible = false; //sign up
+
+                    LinkButton3.Visible = true; // logout
+                    LinkButton7.Visible = true; // username 
+                    LinkButton7.Text = "Hello Admin";
+
+                    //Admin links
+                    LinkButton6.Visible = false; //admin login
+
+                    LinkButton11.Visible = true; //author management 
+                    LinkButton12.Visible = true; //publisher management 
+                    LinkButton8.Visible = true; //bookinventory
+                    LinkButton9.Visible = true; //book issuing
+                    LinkButton10.Visible = true; //member managemt
+                }
+            }
+            catch(Exception ex)
+            {
+                
+            }
+            
+        }
+
+        protected void LinkButton6_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminlogin.aspx");
+        }
+
+        protected void LinkButton11_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminauthor.aspx");
+        }
+
+        protected void LinkButton12_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminpublisher.aspx");
+        }
+
+        protected void LinkButton8_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminbookiventory.aspx");
+        }
+
+        protected void LinkButton9_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminbookissuing.aspx");
+        }
+
+        protected void LinkButton10_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminmember.aspx");
+        }
+
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("userlogin.aspx");
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("signup.aspx");
+        }
+
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+            Session["username"] = "";
+            Session["fullname"] = "";
+            Session["role"] = "";
+            Session["status"] = "";
+
+            //USer links 
+            LinkButton1.Visible = true; //user login
+            LinkButton2.Visible = true; //sign up
+
+            LinkButton3.Visible = false; // logout
+            LinkButton7.Visible = false; // username 
+
+
+            //Admin links
+            LinkButton6.Visible = true; //admin login
+
+            LinkButton11.Visible = false; //author management 
+            LinkButton12.Visible = false; //publisher management 
+            LinkButton8.Visible = false; //bookinventory
+            LinkButton9.Visible = false; //book issuing
+            LinkButton10.Visible = false; //member managemt 
+        }
+    }
+}
---
+++ PortfolioWebASP/PortfolioWebASP/adminpublisher.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="adminpublisher.aspx.cs" Inherits="PortfolioWebASP.adminpublisher" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="card">
+               <div class="card-body">
+                  <div class="row">
+                     <div class="col">
+                        <center>
+                           <h4>Publisher Details</h4>
+                           
+                        </center>
+                     </div>
+                  </div>
+
+                   <div class="row">
+                     <div class="col">
+                        <center>
+                           <img width="100px" src="imgs/publisher.png"/>
+                        </center>
+                     </div>
+                  </div>
+                  <div class="row">
+                     <div class="col">
+                        <hr>
+                     </div>
+                  </div>
+                  <div class="row">
+                     <div class="col-md-4">
+                        <label>Publisher ID</label>
+                        <div class="form-group">
+                            <div class="input-group">
+
+                           <asp:TextBox CssClass="form-control" ID="TextBox1" runat="server" placeholder="ID"></asp:TextBox>
+                            <asp:Button class="btn btn-primary" ID="Button2" runat="server" Text="Go" />
+                            </div>
+                        </div>
+                     </div>
+                     <div class="col-md-8">
+                        <label>Publisher Name</label>
+                        <div class="form-group">
+                           <asp:TextBox CssClass="form-control" ID="TextBox2" runat="server" placeholder="Publisher Name"></asp:TextBox>
+                        </div>
+                     </div>
+                  </div>
+                  
+                  
+                 
+                  
+                  <div class="row">
+                     <div class="col-4">
+                         <asp:Button ID="Button1" runat="server" Text="Add" class="btn btn-lg btn-block btn-success" />
+                     </div>
+                      <div class="col-4">
+                         <asp:Button ID="Button3" runat="server" Text="Update" class="btn btn-lg btn-block btn-warning" />
+                     </div>
+                      <div class="col-4">
+                         <asp:Button ID="Button4" runat="server" Text="Delete" class="btn btn-lg btn-block btn-danger" />
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a href="homepage.aspx"><< Back to Home</a><br><br>
+         </div>
+         <div class="col-md-6">
+            <div class="card">
+               <div class="card-body">
+                  
+                  <div class="row">
+                     <div class="col">
+                        <center>
+                           <h4>Publisher List</h4>
+                           
+                        </center>
+                     </div>
+                  </div>
+
+                   
+
+                  <div class="row">
+                     <div class="col">
+                        <hr>
+                     </div>
+                  </div>
+                  <div class="row">
+                     <div class="col">
+                        <asp:GridView class="table table-striped table-bordered" ID="GridView1" runat="server"></asp:GridView>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+    
+</asp:Content>
