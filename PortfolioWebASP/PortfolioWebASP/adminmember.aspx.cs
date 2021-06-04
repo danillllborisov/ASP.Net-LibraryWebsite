6691daede5ca37afa1c29522ebf130808d19ebf0
danillllborisov <54914198+danillllborisov@users.noreply.github.com>
2021-06-04 13:13:04 -0400

    Member Management
    Back end, gridview

--- PortfolioWebASP/PortfolioWebASP/adminmember.aspx.cs
+++ PortfolioWebASP/PortfolioWebASP/adminmember.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,139 @@ namespace PortfolioWebASP
 {
     public partial class adminmember : System.Web.UI.Page
     {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            GridView1.DataBind();
         }
+        //go
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+            getMemeberById();
+        }
+        //Approve
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            updateUserStatus("Approved");
+        }
+        //pending
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            updateUserStatus("Pending");
+        }
+        //deactivate
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+            updateUserStatus("Disabled");
+        }
+        //delete 
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            deleteMember();
+        }
+
+        void getMemeberById()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT * FROM member_master_tbl WHERE member_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        TextBox2.Text = dr.GetValue(0).ToString();
+                        TextBox7.Text = dr.GetValue(10).ToString();
+                        TextBox8.Text = dr.GetValue(1).ToString();
+                        TextBox3.Text = dr.GetValue(2).ToString();
+                        TextBox4.Text = dr.GetValue(3).ToString();
+                        TextBox9.Text = dr.GetValue(4).ToString();
+                        TextBox10.Text = dr.GetValue(5).ToString();
+                        TextBox11.Text = dr.GetValue(6).ToString();
+                        TextBox6.Text = dr.GetValue(7).ToString();
+
+
+                    }
+                    
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid User');</script>");
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        void updateUserStatus(string status)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("UPDATE member_master_tbl SET account_status = '"+ status +"' WHERE member_id='" + TextBox1.Text.Trim() + "'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                getMemeberById();
+                GridView1.DataBind();
+
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        void deleteMember()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("DELETE FROM member_master_tbl WHERE member_id='" + TextBox1.Text.Trim() + "'", con);
+                
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Author Deleted Successful.');</script>");
+                
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        void clearForm()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox7.Text = "";
+            TextBox8.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox9.Text = "";
+            TextBox10.Text = "";
+            TextBox11.Text = "";
+            TextBox6.Text = "";
+        }
+
     }
 }
