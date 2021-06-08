30efb454ee06169c657c80240d153fd17aeb4b4d
danillllborisov <54914198+danillllborisov@users.noreply.github.com>
2021-06-08 14:51:44 -0400

    Book Details
    Basic back-en functionalities

--- PortfolioWebASP/PortfolioWebASP/adminbookiventory.aspx.designer.cs
+++ PortfolioWebASP/PortfolioWebASP/adminbookiventory.aspx.designer.cs
@@ -186,6 +186,15 @@ namespace PortfolioWebASP
         protected global::System.Web.UI.WebControls.Button Button2;
 
         /// <summary>
+        /// SqlDataSource1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
         /// GridView1 элемент управления.
         /// </summary>
         /// <remarks>
